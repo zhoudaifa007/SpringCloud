e2f6f54ba95c2a78e87663f679fec01245dc4d3f
zhoudaifa007 <zhoudaifa007@gmail.com>
2017-12-16 18:19:13 +0800

    first commit

---
+++ consumer-demo/start.sh
@@ -0,0 +1,3 @@
+java -jar -Dspring.config.location=C:/Users/Frank/Desktop/SpringCloud/application.properties eureka-server-1.0.0.jar
+java -jar -Dspring.config.location=C:/Users/Frank/Desktop/SpringCloud/application1.properties eureka-server-1.0.0.jar
+java -jar -Dspring.config.location=C:/Users/Frank/Desktop/SpringCloud/application2.properties eureka-server-1.0.0.jar
