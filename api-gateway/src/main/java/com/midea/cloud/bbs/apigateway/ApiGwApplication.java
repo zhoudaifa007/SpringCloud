54ba141c4fc2699ca1df14d3a94624e07c5bb6db
zhoudaifa007 <zhoudaifa007@gmail.com>
2017-12-24 10:18:39 +0800

    构建整体工程

---
+++ api-gateway/src/main/java/com/midea/cloud/bbs/apigateway/ApiGwApplication.java
@@ -0,0 +1,19 @@
+package com.midea.cloud.bbs.apigateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringCloudApplication
+public class ApiGwApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(ApiGwApplication.class);
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ApiGwApplication.class).web(true).run(args);
+		logger.info("Api GateWay Started Success!!!");
+	}
+}
